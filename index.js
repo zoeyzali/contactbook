f1a732395aa62abe8910947a1c39c790266422f0
Zoe Ali <zoeyzali@gmail.com>
2019-10-03 22:25:17 +0200

    added icons, fonts and initial styling

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require( 'express' )
+const path = require( 'path' )
 const app = express()
 const port = 3002
 
+// test route 
+app.get( '/test', ( req, res ) => res.send( 'Its been 84 years ;) ' ) )
 
-
-app.get( '/', ( req, res ) => res.send( 'Its been 84 years' )
-)
 app.use( express.static( 'www' ) )
+
 app.listen( port, () => console.log( `Shoo, the server is on port ${port}` ) )
