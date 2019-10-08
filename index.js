77d3fbf4d884195e82e2f690d221ae5babca8e55
Zoe Ali <zoeyzali@gmail.com>
2019-10-08 20:10:59 +0200

    getContacts from json file
    functionality and UI (WIP)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require( 'express' )
-const path = require( 'path' )
+// const path = require( 'path' )
 const app = express()
 const port = 3002
 
 // test route 
 app.get( '/test', ( req, res ) => res.send( 'Its been 84 years ;) ' ) )
 
-app.use( express.static( 'www' ) )
+app.use( express.static( 'public' ) )
 
 app.listen( port, () => console.log( `Shoo, the server is on port ${port}` ) )
